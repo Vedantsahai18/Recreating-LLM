dad2486bc71de57fd27c1b0a0852280021e189c5
vedantsahai18 <vedantsahai18@gmail.com>
2024-06-24 13:30:11 -0400

    mac changes and paramter changed

--- gpt.py
+++ gpt.py
@@ -4,14 +4,15 @@ from torch.nn import functional as F
 
 # hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
+block_size = 8 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "mps" if torch.backends.mps.is_available() else "cpu"
 eval_iters = 200
-n_embd = 384
-n_head = 6
+n_embd = 32
+n_head = 4
 n_layer = 6
 dropout = 0.2
 # ------------
